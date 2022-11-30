3a27e294a057bada479be9701d7d2dd8404dd919
tgu <stevengu2019@hotmail.com>
2022-11-30 16:27:33 +0100

    heap

--- heap.ml
+++ heap.ml
@@ -5,7 +5,7 @@ type tree =
 
 type 'a t = (int * tree) list
 
-
+exception EmptyListe
 let empty = []
 
 let is_singleton h = 
@@ -17,7 +17,22 @@ let is_singleton h =
 let is_empty h = 
   if h = empty then true else false
 
-let add _ _ = failwith "todo"
-let find_min _ = failwith "todo"
+let add e h = 
+  e::h
+  
+let find_min h = 
+  let cmp h1 h2 =
+    if fst h1 < fst h2 then -1
+    else if fst h1 = fst h2 then 0
+    else 1
+  in
+  match List.sort cmp h with
+  []-> raise EmptyListe
+  |e::_ ->e
 
-let remove_min _ = failwith "todo"
+let remove_min h =  
+  let e = try find_min h with EmptyListe-> raise EmptyListe in
+  (e , List.filter (fun x -> x != e) h )
+
+
+  
