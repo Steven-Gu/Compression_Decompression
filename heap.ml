f259b9ad2a72daa872c296f103daafa5e5efd736
tgu <stevengu2019@hotmail.com>
2022-12-07 16:13:27 +0100

    code

--- heap.ml
+++ heap.ml
@@ -1,9 +1,4 @@
-type tree =
-| Leaf of int
-| Node of tree * tree
-
-
-type 'a t = (int * tree) list
+type 'a t = 'a list
 
 exception EmptyListe
 let empty = []
@@ -14,25 +9,21 @@ let is_singleton h =
   |_::[] -> true
   |_::_ -> false
 
-let is_empty h = 
-  if h = empty then true else false
+let is_empty h = h = empty 
 
-let add e h = 
-  e::h
-  
+let rec add e h = 
+    match h with 
+    |[]-> e::[]
+    |p::ll-> if (fst e < fst p) then e::p::ll
+                else p :: add e ll
+
 let find_min h = 
-  let cmp h1 h2 =
-    if fst h1 < fst h2 then -1
-    else if fst h1 = fst h2 then 0
-    else 1
-  in
-  match List.sort cmp h with
-  []-> raise EmptyListe
-  |e::_ ->e
+  if is_empty h then raise EmptyListe
+  else List.hd h
 
-let remove_min h =  
-  let e = try find_min h with EmptyListe-> raise EmptyListe in
-  (e , List.filter (fun x -> x != e) h )
+let remove_min h = 
+  if is_empty h then raise EmptyListe
+  else (List.hd h, List.tl h)
 
-
+  
   
--- huffman.ml
+++ huffman.ml
@@ -1,19 +1,46 @@
 open Bs
+open Heap
 let decompress _ = failwith "todo"
 let compress _ = failwith "todo"
 
+type tree =
+| Leaf of int
+| Node of tree * tree
+
+
 let char_freq f =
-let x = Array.make 256 0 in
-let fo = open_in f in
-let rec loop x is =
-  try
-    let n = input_byte fo in
-    let () = x.(n) <- x.(n) + 1 in
-    loop x is
-  with End_of_file -> x 
-in
-loop x fo
-
-
-let x = char_freq "freq.txt" 
-let () = Array.iter (Printf.printf"%d ") x
+  let x = Array.make 256 0 in
+  let fo = open_in f in
+  let rec loop x is =
+    try
+      let n = input_byte is in
+      let () = x.(n) <- x.(n) + 1 in
+      loop x is
+    with End_of_file -> x 
+  in
+  loop x fo
+   
+  let rec arbre h = 
+    if is_singleton h then snd (List.hd h)
+    else 
+      let x1 = fst (remove_min h) in
+      let x2 = fst (remove_min (snd (remove_min h))) in
+      let h0 = snd (remove_min (snd (remove_min h))) in
+      arbre (add ((fst x1) + (fst x2), Node((snd x1), (snd x2))) h0)
+      
+  let code a =
+    let x = Array.make 256 "" in
+    let l = "" in
+    let rec loop a l =
+      match a with
+      |Leaf(v)-> x.(v) <- l
+      |Node(left,right)->
+        let () = loop left ("0"^l) in loop right ("1"^l) 
+      in
+      let () = loop a l in
+      x 
+  
+  
+  let x = char_freq "freq.txt" 
+  let () = Array.iter (Printf.printf"%d ") x
+  
