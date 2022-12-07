a888c8252e96afe97625c0f968a9552e9f4bd9b2
Hongfei Zhang <hzhang2@a-999903.ups.u-psud.fr>
2022-12-07 15:41:52 +0100

    compress

--- huffman.ml
+++ huffman.ml
@@ -22,13 +22,22 @@ in
 loop x fo
  
 let rec arbre h = 
-  if is_singleton h then snd (hd h)
+  if is_singleton h then snd(List.hd h)
   else 
-    let x1 = snd (remove_min h) in
-    let x2 = snd (remove_min h) in
-    add ((fst x1) + (fst x2),Node((snd x1), (snd x2))) h
-    arbre h
-
+    let x1 = fst (remove_min h) in
+    let x2 = fst (remove_min (snd (remove_min h))) in
+    let h0 = snd (remove_min (snd (remove_min h))) in
+    arbre (add ((fst x1) + (fst x2),Node((snd x1), (snd x2))) h0) 
+    
+let compress f = 
+  let h = char_freq f in 
+  let rec loop h l n = 
+    if n = 256 then l
+    else if h.(n) != 0 then loop h (add (h.(n),Leaf(n)) l) (n+1)
+    else loop h l (n+1)
+  in
+  let l = loop h [] 0 in
+  arbre l
 
 
 let x = char_freq "freq.txt" 
